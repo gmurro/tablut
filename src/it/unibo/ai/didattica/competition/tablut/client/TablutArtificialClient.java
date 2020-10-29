9db27276b215fe60526a85e779961b409e879d87
Giuseppe Murro <g.murro23@gmail.com>
2020-10-29 19:11:41 +0100

    example of movements

--- src/it/unibo/ai/didattica/competition/tablut/client/TablutArtificialClient.java
+++ src/it/unibo/ai/didattica/competition/tablut/client/TablutArtificialClient.java
@@ -1,14 +1,12 @@
 package it.unibo.ai.didattica.competition.tablut.client;
 
-import it.unibo.ai.didattica.competition.tablut.domain.Game;
-import it.unibo.ai.didattica.competition.tablut.domain.GameAshtonTablut;
-import it.unibo.ai.didattica.competition.tablut.domain.State;
-import it.unibo.ai.didattica.competition.tablut.domain.StateTablut;
+import it.unibo.ai.didattica.competition.tablut.domain.*;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class TablutArtificialClient extends TablutClient{
 
@@ -61,10 +59,17 @@ public class TablutArtificialClient extends TablutClient{
         state.setTurn(State.Turn.WHITE);
 
         // set type of game
-        Game rules = new GameAshtonTablut(99, 0, "garbage", "fake", "fake");;
+        Game rules = new GameAshtonTablut(99, 2, "garbage", "white_ai", "black_ai");;
         System.out.println("Ashton Tablut game");
 
+        /* list of position of pawns that color == current player
+         * for WHITE player: white pawns && king
+         * for BALCK player: balck pawns
+        */
         List<int[]> pawns = new ArrayList<int[]>();
+
+        /* list of position of empty cells
+         */
         List<int[]> empty = new ArrayList<int[]>();
 
         // attribute player depends to first parameter passed to main
@@ -89,7 +94,7 @@ public class TablutArtificialClient extends TablutClient{
 
             // TODO CONTROLLA SE QUESTO THREAD E' NECESSARIO
             try {
-                Thread.sleep(1000);
+                Thread.sleep(200);
             } catch (InterruptedException e) {
             }
 
@@ -99,6 +104,72 @@ public class TablutArtificialClient extends TablutClient{
                 // if is my turn (WHITE)
                 if (state.getTurn().equals(StateTablut.Turn.WHITE)) {
                     //TODO WHITE actions
+
+                    int[] buf;
+                    for (int i = 0; i < state.getBoard().length; i++) {
+                        for (int j = 0; j < state.getBoard().length; j++) {
+                            if (state.getPawn(i, j).equalsPawn(State.Pawn.WHITE.toString())
+                                    || state.getPawn(i, j).equalsPawn(State.Pawn.KING.toString())) {
+                                buf = new int[2];
+                                buf[0] = i;
+                                buf[1] = j;
+                                pawns.add(buf);
+                            } else if (state.getPawn(i, j).equalsPawn(State.Pawn.EMPTY.toString())) {
+                                buf = new int[2];
+                                buf[0] = i;
+                                buf[1] = j;
+                                empty.add(buf);
+                            }
+                        }
+                    }
+
+                    // position of pawn selectd to move
+                    int[] selected = null;
+
+                    // if movement is right according to the rules of game
+                    boolean found = false;
+
+                    // movement
+                    Action a = null;
+
+                    // allow movement only horizontal of pawn
+                    int negative = -1;
+
+                    while (!found) {
+
+                        // select last WHITE
+                        selected = pawns.get(8);
+
+                        String from = this.getCurrentState().getBox(selected[0], selected[1]);
+
+                        String to = this.getCurrentState().getBox(selected[0], selected[1] + negative);
+
+                        try {
+                            a = new Action(from, to, State.Turn.WHITE);
+                        } catch (IOException e1) {
+                            // TODO Auto-generated catch block
+                            e1.printStackTrace();
+                        }
+
+                        try {
+                            rules.checkMove(state, a);
+                            found = true;
+                        } catch (Exception e) {
+                            negative *= negative;
+                        }
+
+                    }
+
+                    System.out.println("Mossa scelta: " + a.toString());
+                    try {
+                        this.write(a);
+                    } catch (ClassNotFoundException | IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    pawns.clear();
+                    empty.clear();
+
                 }
 
                 // if is turn of oppenent (BLACK)
@@ -128,6 +199,70 @@ public class TablutArtificialClient extends TablutClient{
                 // if is my turn (BLACK)
                 if (this.getCurrentState().getTurn().equals(StateTablut.Turn.BLACK)) {
                     //TODO BLACK actions
+
+                    int[] buf;
+                    for (int i = 0; i < state.getBoard().length; i++) {
+                        for (int j = 0; j < state.getBoard().length; j++) {
+                            if (state.getPawn(i, j).equalsPawn(State.Pawn.BLACK.toString())) {
+                                buf = new int[2];
+                                buf[0] = i;
+                                buf[1] = j;
+                                pawns.add(buf);
+                            } else if (state.getPawn(i, j).equalsPawn(State.Pawn.EMPTY.toString())) {
+                                buf = new int[2];
+                                buf[0] = i;
+                                buf[1] = j;
+                                empty.add(buf);
+                            }
+                        }
+                    }
+
+                    // position of pawn selectd to move
+                    int[] selected = null;
+
+                    // if movement is right according to the rules of game
+                    boolean found = false;
+
+                    // movement
+                    Action a = null;
+
+                    // allow movement only horizontal of pawn
+                    int negative = -1;
+
+                    while (!found) {
+                        // select E2 BLACK
+                        selected = pawns.get(3);
+
+                        String from = this.getCurrentState().getBox(selected[0], selected[1]);
+
+                        String to = this.getCurrentState().getBox(selected[0], selected[1] + negative);
+
+                        try {
+                            a = new Action(from, to, State.Turn.WHITE);
+                        } catch (IOException e1) {
+                            // TODO Auto-generated catch block
+                            e1.printStackTrace();
+                        }
+
+                        try {
+                            rules.checkMove(state, a);
+                            found = true;
+                        } catch (Exception e) {
+                            negative *= negative;
+                        }
+
+                    }
+
+                    System.out.println("Mossa scelta: " + a.toString());
+                    try {
+                        this.write(a);
+                    } catch (ClassNotFoundException | IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    pawns.clear();
+                    empty.clear();
+
                 }
 
                 // if is turn of oppenent (WHITE)
