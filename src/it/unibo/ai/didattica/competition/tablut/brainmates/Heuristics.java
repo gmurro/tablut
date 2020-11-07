de66fcfea480eb0bd06966e3b68ca0baedcc40be
SalvoPisciotta <salvopisciotta97@gmail.com>
2020-11-07 23:28:22 +0100

    Implimented methods in Heuristics for check if the king goes for win... to test them

--- src/it/unibo/ai/didattica/competition/tablut/brainmates/Heuristics.java
+++ src/it/unibo/ai/didattica/competition/tablut/brainmates/Heuristics.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public abstract class Heuristics {
 
+    //TODO why?
     protected final int[] KING_POSITION = {4,4};
     protected State state;
 
@@ -18,6 +19,10 @@ public abstract class Heuristics {
         return 0;
     }
 
+    /**
+     *
+     * @return the position of the king
+     */
     protected int[] kingPosition(State state) {
         //where I saved the int position of the king
         int[] king= new int[2];
@@ -34,7 +39,10 @@ public abstract class Heuristics {
         return king;
     }
 
-    //method that return if the king is in the Throne or not
+    /**
+     *
+     * @return true if king is on throne, false otherwise
+     */
     public boolean checkKingPosition(State state){
         if(state.getPawn(4,4).equalsPawn("K"))
             return true;
@@ -44,7 +52,10 @@ public abstract class Heuristics {
 
     //TODO OPPONENT PASSATO O NO?
     //TODO dove è circondato
-    //from the state and the position of the pawn I understand how many pawns are near
+    /**
+     *
+     * @return the number of near pawn that are target(BLACK or WHITE)
+     */
     protected int checkNearPawns(State state, int[] position, String target){
         int count=0;
         //GET TURN
@@ -60,7 +71,10 @@ public abstract class Heuristics {
         return count;
     }
 
-    //method to understand where it is occupied near you
+    /**
+     *
+     * @return the positions occupied near the pawn
+     */
     protected List<int[]> positionNearPawns(State state,int[] position, String target){
         List<int[]> occupiedPosition = new ArrayList<int[]>();
         int[] pos = new int[2];
@@ -92,11 +106,18 @@ public abstract class Heuristics {
         return checkNearPawns(state, position, opponent) != 0;
     }
 
-    //Method to understand if I have the king near
+    /**
+     *
+     * @return true if king is near, false otherwise
+     */
     protected boolean checkNearKing(State state, int[] position){
         return checkNearPawns(state, position, "K") > 0;
     }
 
+    /**
+     *
+     * @return how many pawns are in the "strategic" block positions
+     */
     protected int getNumberOfBlockedEscape(){
         int count = 0;
         int[][] blockedEscapes = {{1,1},{1,2},{1,6},{1,7},{2,1},{2,7},{6,1},{6,7},{7,1},{7,2},{7,6},{7,7}};
@@ -121,4 +142,92 @@ public abstract class Heuristics {
         return result;
     }
 
+    protected boolean safePositionKing(State state,int[] kingPosition){
+        if(kingPosition[0] > 2 && kingPosition[0] < 6) {
+            if (kingPosition[1] > 2 && kingPosition[1] < 6) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean kingGoesForWin(State state){
+        int[] kingPosition=this.kingPosition(state);
+        boolean col = false;
+        boolean row = false;
+        if(!safePositionKing(state,kingPosition)){
+            if(!(kingPosition[1] > 2 && kingPosition[1] < 6) && !(kingPosition[0] > 2 && kingPosition[0] < 6)){
+                //yes row yes col
+                col = checkFreeColumn(state, kingPosition);
+                row = checkFreeRow(state,kingPosition);
+            }
+            if(!(kingPosition[0] > 2 && kingPosition[0] < 6)){
+                //no row yes col
+                row = checkFreeRow(state, kingPosition);
+            }
+            if(!(kingPosition[1] > 2 && kingPosition[1] < 6)) {
+                //no row yes col
+                col = checkFreeColumn(state, kingPosition);
+            }
+        return (col || row);
+        }
+        return false;
+    }
+
+    //TODO maybe better return a way to understand were
+    protected boolean checkFreeColumn(State state,int[] position){
+        int row=position[0];
+        int column=position[1];
+        int[] currentPosition = new int[2];
+        boolean freecolumn=true;
+        //going up
+        for(int i = row; i<=8; i++) {
+            currentPosition[0]=i;
+            currentPosition[1]=column;
+            if (checkOccupiedPosition(state,currentPosition)) {
+                freecolumn = false;
+            }
+        }
+        //going down
+        for(int i=row;i>=0;i--) {
+            currentPosition[0]=i;
+            currentPosition[1]=column;
+            if (checkOccupiedPosition(state,currentPosition)) {
+                freecolumn = false;
+            }
+        }
+        return freecolumn;
+    }
+
+    //TODO maybe better return a way to understand were
+    protected boolean checkFreeRow(State state,int[] position){
+        int row=position[0];
+        int column=position[1];
+        int[] currentPosition = new int[2];
+        boolean freeRow=true;
+        //going right
+        for(int i=column;i<=8;i++) {
+            currentPosition[0]=row;
+            currentPosition[1]=i;
+            if (checkOccupiedPosition(state,currentPosition)) {
+                freeRow = false;
+            }
+        }
+        //going left
+        for(int i=row;i>=0;i--) {
+            currentPosition[0]=row;
+            currentPosition[1]=i;
+            if (checkOccupiedPosition(state,currentPosition)) {
+                freeRow = false;
+            }
+        }
+        return freeRow;
+    }
+
+    public boolean checkOccupiedPosition(State state,int[] position){
+        if(state.getPawn(position[0],position[1]).equals(State.Pawn.EMPTY))
+            return false;
+        else
+            return true;
+        }
 }
